e1ab17d5559ad9516c098a9532e70d649fff1ba7
youssef <yousseftaoussi894@gmail.com>
2024-07-13 07:19:24 +0100

    .

--- src/assets/data/skills.js
+++ src/assets/data/skills.js
@@ -13,5 +13,6 @@ const skills = [
   "https://img.icons8.com/?size=100&id=aqb9SdV9P8oC&format=png&color=000000",
   "https://img.icons8.com/?size=100&id=74402&format=png&color=000000",
   "https://img.icons8.com/?size=100&id=25Sjy8fKExYA&format=png&color=000000",
+  "https://icons8.com/icon/Ign0QG33bZzq/sql",
 ];
 export default skills;
